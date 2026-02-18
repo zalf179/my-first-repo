fda70e9df567dd15c1604eb1a731f92e5d8b8d72
4th3nA <indofrick@gmail.com>
2026-02-18 10:52:49 +0700

    freaky ahh cat

---
+++ freakycat.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from PIL import Image, ImageTk, ImageSequence
+import requests
+from io import BytesIO
+
+# URL GIF kamu
+GIF_URL = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnJuOWRjOXljM2x0dXp3bG01aWxtYTZ6c3l2cWhyZGg0cXRpaGdtZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vPzbDN4rBxuvtpSpzF/giphy.gif"
+
+# Ambil GIF dari internet
+resp = requests.get(GIF_URL)
+gif_bytes = BytesIO(resp.content)
+
+# Buka GIF pakai Pillow
+pil_gif = Image.open(gif_bytes)
+
+# Setup window
+root = tk.Tk()
+root.title("FREAKYYY CATTT 🤪🤪")
+root.geometry("400x400")
+root.configure(bg="black")
+
+label = tk.Label(root, bg="black")
+label.pack(expand=True)
+
+frames = []
+for frame in ImageSequence.Iterator(pil_gif):
+    frames.append(ImageTk.PhotoImage(frame))
+
+def animate(index):
+    label.config(image=frames[index])
+    root.after(100, animate, (index + 1) % len(frames))
+
+# Mulai animasi
+root.after(0, animate, 0)
+root.mainloop()
